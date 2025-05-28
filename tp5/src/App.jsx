ae66e57b6822deed86c0b28c34ee33ddb9045af7
Leandro794 <44125429@fi.unju.edu.ar>
2025-05-28 18:45:17 -0300

    feat: Creación del navbar para moverse entre paginas [Leandro Bejarano]

--- tp5/src/App.jsx
+++ tp5/src/App.jsx
@@ -1,35 +1,35 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [alumno, setAlumno] = useState({
+    lu: '',
+    nombre: '',
+    apellido: '',
+    curso: '',
+    email: '',
+    domicilio: '',
+    teléfono: '',
+  })
+  const [alumnos, setAlumnos] = useState([]);
   return (
-    <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <BrowserRouter>
+          <nav>
+            <ul>
+              <li><Link to='/inicio'>Pagina Principal</Link> </li>
+              <li><Link to='/nuevo'>Registrar alumno</Link> </li>
+              <li><Link to='/lista'>Lista de alumnos</Link> </li>
+              <li><Link to='/acerca'>Acerca de nosotros</Link> </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path='/inicio' element={<Inicio/>}></Route>
+            <Route path='/nuevo' element={<NuevoAlumno/>}></Route>
+            <Route path='/acerca' element={<AcercaDe/>}></Route>
+          </Routes>
+        </BrowserRouter>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  );
 }
 
 export default App
