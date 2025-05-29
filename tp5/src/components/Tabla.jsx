94b156d78c6458c840342a6996f511e8bdbb4bc1
Leandro794 <44125429@fi.unju.edu.ar>
2025-05-28 22:52:39 -0300

    feat: Añadido de la opción editar a la tabla [Leandro Bejarano]

--- tp5/src/components/Tabla.jsx
+++ tp5/src/components/Tabla.jsx
@@ -1,3 +1,5 @@
+import {Link} from 'react-router-dom'
+
 function Tabla({alumnos}) {
     return (
         <table>
@@ -10,11 +12,12 @@ function Tabla({alumnos}) {
                     <th>Curso</th>
                     <th>Telefono</th>
                     <th>Domicilio</th>
+                    <th></th>
                 </tr>
             </thead>
             <tbody>
-                {alumnos.map((a,i) => (
-                    <tr key={i}>
+                {alumnos.map((a) => (
+                    <tr key={a.lu}>
                         <td>{a.lu}</td>
                         <td>{a.nombre}</td>
                         <td>{a.apellido}</td>
@@ -22,6 +25,7 @@ function Tabla({alumnos}) {
                         <td>{a.curso}</td>
                         <td>{a.telefono}</td>
                         <td>{a.domicilio}</td>
+                        <td><Link to={`/lista/${a.lu}/editar`}>Editar</Link></td>
                     </tr>
                 ))}
             </tbody>
