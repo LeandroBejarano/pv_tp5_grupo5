272e11eb3fbbe29f0584bf9dce16a510a3ea559a
Leandro794 <44125429@fi.unju.edu.ar>
2025-05-29 00:23:11 -0300

    feat: Incorporacion del boton eliminar utilizando evento nativo addEventListener [Leandro Bejarano]

--- tp5/src/components/Tabla.jsx
+++ tp5/src/components/Tabla.jsx
@@ -1,23 +1,57 @@
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { useEffect, useRef } from 'react';
 
-function Tabla({alumnos}) {
+function Tabla({ alumnos, setAlumnos }) {
+    const botonesRef = useRef([]);
+    const handlersRef = useRef([]);
+
+    useEffect(() => {
+        botonesRef.current.forEach((btn, i) => {
+            if (btn && handlersRef.current[i]) {
+                btn.removeEventListener("click", handlersRef.current[i]);
+            }
+        });
+
+        botonesRef.current.forEach((btn, i) => {
+            if (btn) {
+                const handler = () => {
+                    const confirmar = confirm("¿Deseás eliminar este alumno?");
+                    if (confirmar) {
+                        setAlumnos(prev => prev.filter(a => a.lu !== alumnos[i].lu));
+                    }
+                };
+                handlersRef.current[i] = handler;
+                btn.addEventListener("click", handler);
+            }
+        });
+
+        return () => {
+            botonesRef.current.forEach((btn, i) => {
+                if (btn && handlersRef.current[i]) {
+                    btn.removeEventListener("click", handlersRef.current[i]);
+                }
+            });
+        };
+    }, [alumnos, setAlumnos]);
+
     return (
         <table>
             <thead>
                 <tr>
                     <th>LU</th>
-                    <th>Nombre/s</th>
-                    <th>Apellido/s</th>
+                    <th>Nombre</th>
+                    <th>Apellido</th>
                     <th>E-mail</th>
                     <th>Curso</th>
-                    <th>Telefono</th>
+                    <th>Teléfono</th>
                     <th>Domicilio</th>
+                    <th></th>
                     <th></th>
                     <th></th>
                 </tr>
             </thead>
             <tbody>
-                {alumnos.map((a) => (
+                {alumnos.map((a, i) => (
                     <tr key={a.lu}>
                         <td>{a.lu}</td>
                         <td>{a.nombre}</td>
@@ -28,11 +62,12 @@ function Tabla({alumnos}) {
                         <td>{a.domicilio}</td>
                         <td><Link to={`/lista/${a.lu}/editar`}>Editar</Link></td>
                         <td><Link to={`/lista/${a.lu}`}>Ver</Link></td>
+                        <td><button ref={el => botonesRef.current[i] = el}>Eliminar</button></td>
                     </tr>
                 ))}
             </tbody>
         </table>
-    )
+    );
 }
 
-export default Tabla
+export default Tabla;
