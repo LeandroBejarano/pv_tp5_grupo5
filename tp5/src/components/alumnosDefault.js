3463856e1cac973e26d0c2a8aa2eceba401e3743
Leandro794 <44125429@fi.unju.edu.ar>
2025-05-30 19:45:28 -0300

    feat: Creo una funcion para generar una lista [Leandro Bejarano]

---
+++ tp5/src/components/alumnosDefault.js
@@ -0,0 +1,33 @@
+function alumnosDefault() {
+    return [
+        {
+            lu: 'APU00001',
+            nombre: 'Leandro',
+            apellido: 'Bejarano',
+            curso: '2',
+            email: 'leandro@gmail.com',
+            domicilio: 'Calle 525',
+            telefono: '123456789'
+        },
+        {
+            lu: 'APU00002',
+            nombre: 'Tobias',
+            apellido: 'Torres',
+            curso: '2',
+            email: 'torrestob@gmail.com',
+            domicilio: 'Avenida Siempreviva 742',
+            telefono: '987654321'
+        },
+        {
+            lu: 'APU00003',
+            nombre: 'Daniel',
+            apellido: 'Rearte',
+            curso: '2',
+            email: 'satan0600@example.com',
+            domicilio: 'Calle Los Laureles 456',
+            telefono: '1122334455'
+        }
+    ];
+}
+
+export default alumnosDefault
